ac0f44c99a2144ce6ea27288033a8614d3fb84a4
razertory <razertory@163.com>
2018-03-08 16:54:47 +0800

    1st commit

---
+++ create_chinese_provices.sql
@@ -0,0 +1,42 @@
+CREATE TABLE S_Province(
+	ProvinceID bigint NOT NULL PRIMARY KEY ,
+	ProvinceName VARCHAR( 50 ) NULL,
+	DateCreated datetime NULL,
+	DateUpdated datetime NULL
+)ENGINE = MYISAM DEFAULT CHARSET=utf8 ;
+
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (1, '北京市', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (2, '天津市', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (3, '河北省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (4, '山西省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (5, '内蒙古自治区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (6, '辽宁省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (7, '吉林省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (8, '黑龙江省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (9, '上海市', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (10, '江苏省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (11, '浙江省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (12, '安徽省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (13, '福建省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (14, '江西省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (15, '山东省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (16, '河南省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (17, '湖北省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (18, '湖南省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (19, '广东省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (20, '广西壮族自治区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (21, '海南省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (22, '重庆市', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (23, '四川省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (24, '贵州省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (25, '云南省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (26, '西藏自治区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (27, '陕西省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (28, '甘肃省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (29, '青海省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (30, '宁夏回族自治区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (31, '新疆维吾尔自治区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (32, '香港特别行政区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (33, '澳门特别行政区', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+INSERT S_Province (ProvinceID, ProvinceName, DateCreated, DateUpdated) VALUES (34, '台湾省', CAST(0x00009A28014555BE AS DateTime), CAST(0x00009A28014555BE AS DateTime));
+
